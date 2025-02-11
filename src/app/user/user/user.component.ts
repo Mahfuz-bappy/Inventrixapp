a53e277a751d30c82a9b39a474a6c9fb6477ddbb
Mahfuz-bappy <md.mahfuzbappy@gmail.com>
2025-02-11 18:57:07 +0600

    user layout animation added

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import {  RouterModule } from '@angular/router';
+import {  ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-user',
-  imports: [RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+    animations: [
+      trigger('routerFadeIn', [
+        transition('* <=> *', [
+          query(':enter', [
+            style({ opacity: 0 }),
+            animate('1s ease-in-out', style({ opacity: 1 }))
+          ], { optional: true }),
+        ])
+      ])
+    ]
 })
-export class UserComponent {
+export class UserComponent { 
+  constructor(private context: ChildrenOutletContexts) { }
+
+  getRouteUrl() {
+    return this.context.getContext('primary')?.route?.url;
+  }
 
 }
