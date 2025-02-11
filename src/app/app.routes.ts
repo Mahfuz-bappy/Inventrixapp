903f2b3f85d252b13141de79c1096471aacf8b55
Mahfuz-bappy <md.mahfuzbappy@gmail.com>
2025-02-11 18:12:05 +0600

    created user login register component and configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { PageNotFoundComponent } from './wildNavigate/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',   component: UserComponent,  // Default route
+        children: [  
+            // Child routes for login and register
+          {
+            path: 'login', component: LoginComponent
+          },
+          {
+            path: 'register', component: RegisterComponent
+          },
+        ]
+    },
+    {
+        path: '**', component: PageNotFoundComponent    // Wildcard route for a 404 page
+    }
+    
+
+];
