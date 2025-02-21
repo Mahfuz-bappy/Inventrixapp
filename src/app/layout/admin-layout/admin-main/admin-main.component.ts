f3e3bddeab40d533c17631ebac8a7b7c404268a6
Mahfuz-bappy <md.mahfuzbappy@gmail.com>
2025-02-22 03:09:33 +0600

    adjust sidemenu toggle with main menu

--- src/app/layout/admin-layout/admin-main/admin-main.component.ts
+++ src/app/layout/admin-layout/admin-main/admin-main.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,5 +10,5 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./admin-main.component.css'] // Updated style URL
 })
 export class AdminMainComponent {
-  // Component logic here
+  @Input() isSidebarCollapsed: boolean = false;
 }
